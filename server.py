d7fbabdd5d6fcf7fed40c83b395e4d0043b46aba
chaitanyachavali <chavalichaithanyachinna@gmail.com>
2018-02-02 19:08:53 +0530

    Notify not occuring on function call

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-import gi
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify, GdkPixbuf
+# import gi
+# gi.require_version('Notify', '0.7')
+# from gi.repository import Notify, GdkPixbuf
 from imapclient import IMAPClient
 import time
+import subprocess
 
-DEBUG = False
+DEBUG = True
 
 HOSTNAME = 'imap.gmail.com'
 USERNAME = ''
@@ -16,17 +17,19 @@ MAILBOX = 'Inbox'
 NEWMAIL_OFFSET = 0
 MAIL_CHECK_FREQ = 2
 NOTIFY_ON_NO_INT_CONN = False
-NOTIFY_IMAGE = '/home/chaitanya/Documents/Github/email-notifier/favicon.ico'
-Notify.init("Email Notifier")
-emailNotification = Notify.Notification.new("INIT", "INIT", image)
+# NOTIFY_IMAGE = '/home/chaitanya/Documents/Github/email-notifier/favicon.ico'
+# Notify.init("Email Notifier")
+# emailNotification = Notify.Notification.new("INIT", "INIT", NOTIFY_IMAGE)
 
 def notifyUser(emailCount):
-    checkTag = 'mail' if emailCount == 1 else 'mails'
-    headTag = "Hey there! New " + checkTag + " for you"
-    bodyTag = "You have " + str(emailCount) + " new " + checkTag + " in your mailbox"
-    emailNotification.update(headTag, boadyTag, image)
-    emailNotification.show()
-    time.sleep(4)
+    print("Notifying")
+    subprocess.Popen(['notify-send', 'Title', 'Message'])
+    # checkTag = 'mail' if emailCount == 1 else 'mails'
+    # headTag = "Hey there! New " + checkTag + " for you"
+    # bodyTag = "You have " + str(emailCount) + " new " + checkTag + " in your mailbox"
+    # emailNotification.update(headTag, boadyTag, image)
+    # emailNotification.show()
+    time.sleep(5)
 
 try:
     while True:
