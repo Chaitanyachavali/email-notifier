d54faf2625a7d60471a28f02cf83049eee913895
chaitanyachavali <chavalichaithanyachinna@gmail.com>
2018-02-02 18:15:54 +0530

    except keyboard interupt

--- server.py
+++ server.py
@@ -6,28 +6,35 @@ import time
 DEBUG = True
 
 HOSTNAME = 'imap.gmail.com'
-USERNAME = 'chavalichaithanyachinna@gmail.com'
-PASSWORD = 'chavali@chinna'
+USERNAME = ''
+PASSWORD = ''
 MAILBOX = 'Inbox'
 
-NEWMAIL_OFFSET = 0   # my unread messages never goes to zero, yours might
-MAIL_CHECK_FREQ = 2 # check mail every 60 seconds
+NEWMAIL_OFFSET = 0
+MAIL_CHECK_FREQ = 2
+NOTIFY_ON_NO_INT_CONN = False
 
-while True:
-    try:
-        server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
-        server.login(USERNAME, PASSWORD)
+try:
+    while True:
+        try:
+            server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
+            server.login(USERNAME, PASSWORD)
 
-        if DEBUG:
-            print('Logging in as ' + USERNAME)
-            select_info = server.select_folder(MAILBOX)
-            print('%d messages in INBOX' % select_info[b'EXISTS'])
+            if DEBUG:
+                print('Logging in as ' + USERNAME)
+                select_info = server.select_folder(MAILBOX)
+                print('%d messages in INBOX' % select_info[b'EXISTS'])
 
-        folder_status = server.folder_status(MAILBOX, 'UNSEEN')
-        newmails = int(folder_status[b'UNSEEN'])
+            folder_status = server.folder_status(MAILBOX, 'UNSEEN')
+            newmails = int(folder_status[b'UNSEEN'])
 
-        if DEBUG:
-            print("You have %d new emails" % newmails)
-        time.sleep(MAIL_CHECK_FREQ)
-    except Exception as e:
-        print(e)
+            if DEBUG:
+                print("You have %d new emails" % newmails)
+            time.sleep(MAIL_CHECK_FREQ)
+        except Exception as e:
+            if NOTIFY_ON_NO_INT_CONN:
+                print(e)
+            else:
+                pass
+except KeyboardInterrupt:
+    print("Service being closed by user")
