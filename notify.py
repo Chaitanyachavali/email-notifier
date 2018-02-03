1a66bfa956503afc0ad7051c77f001d539c73095
chaitanyachavali <chavalichaithanyachinna@gmail.com>
2018-02-03 17:03:10 +0530

    fixing recurral notifications error

--- notify.py
+++ notify.py
@@ -11,25 +11,18 @@ import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify, GdkPixbuf
 import time
-
-
-# One time initialization of libnotify
 Notify.init("Email Notifier")
-
-# Create the notification object
 summary = "New email"
 body = "you have new emails"
 image = "/home/chaitanya/Documents/Github/email-notifier/favicon.ico"
 emailNotification = Notify.Notification.new(summary,
     body, image
 )
-# image = GdkPixbuf.Pixbuf.new_from_file("/home/chaitanya/Documents/Github/email-notifier/favicon.ico")
-# notification.set_icon_from_pixbuf(image)
-# notification.set_image_from_pixbuf(image)
-# Actually show on screen
-emailNotification.show()
-
-time.sleep(5)
-
-emailNotification.update("New Up", "up up", image)
-emailNotification.show()
+# emailNotification.show()
+# time.sleep(5)
+i=0
+while True:
+	i=i+1
+	emailNotification.update(str(i), "new", image)
+	emailNotification.show()
+	time.sleep(5)
--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# import gi
-# gi.require_version('Notify', '0.7')
-# from gi.repository import Notify, GdkPixbuf
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify, GdkPixbuf
 from imapclient import IMAPClient
 import time
 import subprocess
@@ -10,26 +10,29 @@ import subprocess
 DEBUG = True
 
 HOSTNAME = 'imap.gmail.com'
-USERNAME = ''
-PASSWORD = ''
+USERNAME = 'chavalichaithanyachinna@gmail.com'
+PASSWORD = 'chavali@chinna'
 MAILBOX = 'Inbox'
 
 NEWMAIL_OFFSET = 0
 MAIL_CHECK_FREQ = 2
-NOTIFY_ON_NO_INT_CONN = False
-# NOTIFY_IMAGE = '/home/chaitanya/Documents/Github/email-notifier/favicon.ico'
-# Notify.init("Email Notifier")
-# emailNotification = Notify.Notification.new("INIT", "INIT", NOTIFY_IMAGE)
+NOTIFY_ON_NO_INT_CONN = True
+NOTIFY_IMAGE = '/home/chaitanya/Documents/Github/email-notifier/favicon.ico'
+
 
 def notifyUser(emailCount):
+    global NOTIFY_IMAGE
+    Notify.init("Email Notifier")
+    emailNotification = Notify.Notification.new("INIT", "INIT", NOTIFY_IMAGE)
+    emailNotification.show()
     print("Notifying")
-    subprocess.Popen(['notify-send', 'Title', 'Message'])
-    # checkTag = 'mail' if emailCount == 1 else 'mails'
-    # headTag = "Hey there! New " + checkTag + " for you"
-    # bodyTag = "You have " + str(emailCount) + " new " + checkTag + " in your mailbox"
-    # emailNotification.update(headTag, boadyTag, image)
-    # emailNotification.show()
+    checkTag = 'mail' if emailCount == 1 else 'mails'
+    headTag = "Hey there! New " + checkTag + " for you"
+    bodyTag = "You have " + str(emailCount) + " new " + checkTag + " in your mailbox"
+    emailNotification.update(headTag, bodyTag, NOTIFY_IMAGE)
+    emailNotification.show()
     time.sleep(5)
+    Notify.uninit()
 
 try:
     while True:
@@ -38,7 +41,7 @@ try:
             gServer.login(USERNAME, PASSWORD)
 
             if DEBUG:
-                print('Logging in as ' + USERNAME)
+                print('Logged in as ' + USERNAME)
                 select_info = gServer.select_folder(MAILBOX)
                 print('%d messages in INBOX' % select_info[b'EXISTS'])
 
